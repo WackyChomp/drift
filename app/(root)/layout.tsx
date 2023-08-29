59c773f777ccd10b932c7c45b408d46d98e96d98
Wilson Chan <wilsonchan54@gmail.com>
2023-08-28 22:28:39 -0400

    Fixed sign-in auth by wrapping html with ClerkProvider component / removed out global.css in sign-up layout / established basic root homepage layout / created components folder

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs";
 
+import Navbar from '@/components/shared/Navbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Footer from '@/components/shared/Footer';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +24,25 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+
+          <Navbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            
+            <RightSidebar />
+          </main>
+
+          <Footer />
+
+        </body>
       </html>
     </ClerkProvider>
   )
